cda154976f8e6116b1008614cf8c608f178ed5a2
Edward Bako <edward.bako@gmail.com>
2017-10-14 01:06:49 +0700

    Change mail link

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,6 +1,6 @@
 class MailController < ApplicationController
 
   def index
-    redirect_to 'https://mail.yandex.ru/for/optika-style'
+    redirect_to 'https://mail.yandex.ru/for/optika-style.ru'
   end
 end
